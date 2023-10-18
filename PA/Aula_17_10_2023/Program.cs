ff5833223027573bb0b77f039977531605cf8880
Aluno <aluno@etec.sp.gov.br>
2023-10-17 22:10:52 -0300

    atividade aula 17/10

---
+++ PA/Aula_17_10_2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_17_10_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
